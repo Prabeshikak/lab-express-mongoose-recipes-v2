d8eb57a1d71cdddd5c0482b0ed8d1b1ae1945041
Prabeshika <prabeshika14@gmail.com>
2024-01-17 14:28:55 +0100

    Mongoose express recipe

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 const app = express();
 
 // MIDDLEWARE
@@ -8,44 +9,114 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
-
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
-
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", async (request, response) => {
+  const payload = request.body;
+  try {
+    const newRecipe = await Recipe.create(payload);
+    response.status(201).json(newRecipe);
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message: "Internal Server Error while creating a new recipe",
+    });
+  }
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", async (request, response) => {
+  try {
+    const allRecipes = await Recipe.find();
+    response.status(200).json(allRecipes);
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message: "Internal Server Error while getting all the recipes",
+    });
+  }
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:id", async (request, response) => {
+  try {
+    const recipeId = request.params.id;
+    const oneRecipe = await Recipe.findById(recipeId);
+    if (!oneRecipe) {
+      return response.status(404).json({ message: "Recipe not found" });
+    }
+    response.status(200).json(oneRecipe);
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message: "Internal Server Error while getting the recipe",
+    });
+  }
+});
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put("/recipes/:id", async (request, response) => {
+  try {
+    const recipeId = request.params.id;
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      recipeId,
+      request.body,
+      {
+        new: true,
+      }
+    );
+    if (!updatedRecipe) {
+      return response.status(404).json({ message: "Recipe not found" });
+    }
+    response.status(200).json(updatedRecipe);
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message: "Internal Server Error while updating the recipe",
+    });
+  }
+});
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
-
+app.delete("/recipes/:id", async (request, response) => {
+  try {
+    const recipeId = request.params.id;
+    const deletedRecipe = await Recipe.findByIdAndDelete(recipeId);
+    if (!deletedRecipe) {
+      return response.status(404).json({ message: "Recipe not found" });
+    }
+    response.status(204).send();
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message: "Internal Server Error while deleting the recipe",
+    });
+  }
+});
 
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
+app.listen(5005, () => console.log("My first app listening on port 5005!"));
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,28 @@
-// Your code here ...
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//Created a Schema
+
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  instructions: { type: String, required: true },
+  level: {
+    type: String,
+    enum: ["EasyPeasy", "Amateur Chef ", "UltraPro Chef"],
+  },
+  ingredients: { type: [String] },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
+  isArchived: { type: Boolean, default: false },
+  created: { type: Date, default: Date.now },
+});
+
+//Created a Model
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
